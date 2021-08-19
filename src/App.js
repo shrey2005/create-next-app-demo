6ca54ddb78eca9995efc88a5feb93ee7e5f7012f
shrey2005 <54803537+shrey2005@users.noreply.github.com>
2021-08-19 13:25:46 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -22,6 +22,7 @@ class LambdaDemo extends Component {
 
     return (
       <p>
+        <h3>This is demo using netlify using react js</h3>
         <button onClick={this.handleClick("hello")}>{loading ? "Loading..." : "Call Lambda"}</button>
         <button onClick={this.handleClick("async-dadjoke")}>{loading ? "Loading..." : "Call Async Lambda"}</button>
         <br />
